fc88fe020334aeec3c01d1aa5b84ae39deba8307
marliolimpiojw-gif <marliolimpiojw@gmail.com>
2025-09-12 20:27:52 +0000

    variaveis concluidas/char,float,intAdiciona código principal e configurações do projeto

---
+++ #include<stdio.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+
+int main ()
+
+{    
+//Declaraçâo e inicialização de variaveis do tipo  strings 
+
+char cidade_1 [8] = "vitoria";
+char cidade_2 [9] = " Aracaju ";
+char estado_1 [15] = "Espirito Santo";
+char estado_2 [9] = "Sergipe";
+char codigo_1 [8] = "cod_27";
+char codigo_2 [9] =  "cod_79";
+
+printf  ("informe o estado1:/n");
+scanf ("%s", estado_1 );
+printf ("informe o estado2 :/n");
+scanf ("%s" , estado_2);
+
+printf ("Informe a cidade1 :/n");
+scanf("%s", cidade_1 );
+printf  ("Informe a cidade2 :/n");
+scanf ("%s", cidade_2);
+
+printf ("Informe o codigo da cidade1:/n ");
+scanf ("%s",cod_27");
+printf ("Informe o codigo da cidade2:/n");
+scanf ("%s", cod_79);
+
+// Declaração e inicializacão do tipo inteiro
+
+int  população1 = 4.000.000, população2 = 2.300.000;
+int  ponto turistico 1 = 80, ponto turistico 2 = 60;
+
+printf ("Informe a população 1 :/n");
+scanf ("%d", &população 1 );
+printf ("Informe a população 2 :/n ");
+scanf ("%d", &população2 );
+
+printf ("Informe quantidade de ponto turistico 1:");
+scanf ("%d", &ponto turistico 1);
+printf ("Informe quantidade de ponto turistico 2:");
+scanf ("%d", &ponto turistico 2);
+
+// Declaração e inicialização de variaveis tipo float
+
+float  Area 1 = 46.074.448,  Area 2 = 21.938.188; 
+float  PIB 1 = 59,5 b,    PIB 2 = 11,7 b;
+float  Densidade populacional 1 =89,03 Km², Densidade populaciona 2= 100,74Km²
+
+
+printf  (" Informe a area 1 :/n");
+scanf ("%f", &area 1 );
+printf ("Informe a area 2:/n");
+scanf ("%f", &area 2 );
+
+printf ("Informe o PIB 1:/n");  
+scanf ("%"f", PIB 1 );
+printf (" Informe o PIB 2:/n");
+scanf ("%f", PIB 2)
+
+printf ("Informe a densidade populaciona l: /n ");
+scanf (" %f', &densidade populacional);
+printf ("informe a densidade populacional 2:/n") ;
+ scanf ("%f", &densidade populacional 2); 
+
+
+
+
+
+
+
+
+ return 0;
+}
+
+
+
+
+
+
+
