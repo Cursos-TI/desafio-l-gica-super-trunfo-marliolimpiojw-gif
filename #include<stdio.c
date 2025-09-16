7b4b18882533c9c4f5ca33e710caa846a5925444
marliolimpiojw-gif <marliolimpiojw@gmail.com>
2025-09-16 18:46:42 +0000

    char

--- #include<stdio.c
+++ #include<stdio.c
@@ -1,38 +1,39 @@
 #include<stdio.h>
+#include<stdio.c>
 
 int main ()
 
 {    
 //Declaraçâo e inicialização de variaveis do tipo  strings 
 
-char cidade_1 [8] = "Vitoria";
-char cidade_2 [9] = "Aracaju";
-char estado_1 [15] = "Espirito_Santo";
-char estado_2 [9] = "Sergipe";
-char codigo_1 [8] = "cod_27";
-char codigo_2 [9] = "cod_79";
+char Cidade_1 [8] = "Vitoria";
+char Cidade_2 [9] = "Aracaju";
+char Estado_1 [15] = "Espirito_Santo";
+char Estado_2 [9] = "Sergipe";
+char Codigo_1 [8] = "Cod_27";
+char Codigo_2 [9] = "Cod_79";
 
-printf ("Informe o estado1 :/n");
-scanf ("%s", estado_1 );
-printf ("o estado_1 informado e: %s\n", estado_1);
-printf ("informe o estado2 :/n");
-scanf ("%s", estado_2);
-printf("o estado_2 informado e: %s\n", estado_2);
+printf ("Informe o Estado_1 :");
+scanf ("%s",Estado_1);
+printf ("O Estado_1 informado e: %s",Estado_1);
+printf ("Informe o Estado_2 :/n");
+scanf ("%s",Estado_2);
+printf("O Estado_2 informado e: %s",Estado_2);
 
-printf ("Informe a cidade_1:/n");
-scanf("%s", cidade_1 );
-printf(" a cidade_1 informada e: %s\n", cidade_1);
+printf ("Informe a Cidade_1:/n");
+scanf("%s", Cidade_1);
+printf(" a Cidade_1 informada e: %s", Cidade_1);
 
-printf  ("Informe a cidade_2:/n");
-scanf ("%s", cidade_2);
-printf (" a cidade_2 informada e: %s\n", cidade_2);
+printf  ("Informe a Cidade_2:/n");
+scanf ("%s", Cidade_2);
+printf ("a Cidade_2 informada e: %s", Cidade_2);
 
 printf ("Informe o codigo da cidade_1:/n ");
-scanf ("%s", codigo_1);
-printf ("o codigo da cidade_1 informado e: %s\n", codigo_1);
-printf ("Informe o codigo da cidade_2:/n");
-scanf ("%s", codigo_2);
-printf ("o codigo da cidade_2 informado e: %s\n", codigo_2);
+scanf ("%s", Codigo_1);
+printf ("o Codigo da cidade_1 informado e: %s", Codigo_1);
+printf ("Informe o Codigo da Cidade_2:/n");
+scanf ("%s", Codigo_2);
+printf ("o Codigo da Cidade_2 informado e: %s", Codigo_2);
 
 
 // Declaração e inicializacão do tipo inteiro
@@ -40,52 +41,53 @@ printf ("o codigo da cidade_2 informado e: %s\n", codigo_2);
 int  populacao1 = 4000000, populacao2 = 2300000;
 int  ponto_turistico_1 = 80, ponto_turistico_2 = 60;
 
-printf ("Informe a populacao1 :\n");
-scanf ("%d", &populacao1 );
-printf ("A populacao 1 informada e: %d\n", populacao1);
+printf ("Informe a populacao1 :");
+scanf ("%i", &populacao1 );
+printf ("A populacao 1 informada e: %i", populacao1);
 
-printf ("Informe a populacao 2 :\n");
-scanf ("%d", &populacao2 );
-printf ("A populacao 2 informada e: %d\n", populacao2);
+printf ("Informe a populacao 2 :");
+scanf ("%i", &populacao2 );
+printf ("A populacao 2 informada e: %i", populacao2);
 
-printf ("Informe quantidade de ponto turistico 1:\n");
-scanf ("%d", &ponto_turistico_1);
-printf ("A quantidade de ponto turistico 1 informada e: %d\n", ponto_turistico_1);
-printf ("Informe quantidade de ponto turistico 2:\n");
-scanf ("%d", &ponto_turistico_2);
-printf ("A quantidade de ponto turistico 2 informada e: %d\n", ponto_turistico_2);
+printf ("Informe quantidade de ponto_turistico_1:");
+scanf ("%i", &ponto_turistico_1);
+printf ("A quantidade de ponto_turistico_1 informada e: %i", ponto_turistico_1);
+printf ("Informe quantidade de ponto_turistico_2:");
+scanf ("%i", &ponto_turistico_2);
+
+printf ("A quantidade de ponto_turistico_2 informada e: %i", ponto_turistico_2);
 
 // Declaração e inicialização de variaveis tipo float 
 
 float   Area_1 = 45000.00,  Area_2 = 21200.00;
 
-float  PIB_1 = 5.0,    PIB_2 = 1.8;
+float  PIB_1 = 59,5,    PIB_2 = 11,7;
 
-float  Densidade_populacional_1 =1.2, Densidade_populacional_2 = 1.1;
+float  Densidade_populacional_1 = 89,03, Densidade_populacional_2 = 100,74;
 
 printf  ("Informe a Area_1 :\n");
 scanf ("%f", &Area_1);
-printf ("Exibindo valor real de Area_1 : %f\n", Area_1);
+printf ("Exibindo valor real de Area_1 : %. 2f \n",Area_1);
 
 printf ("Informe a Area_2:\n");
 scanf ("%f", &Area_2);
-printf ("Exibindo valor real de Area_2 : %f\n", Area_2);
+printf ("Exibindo valor real de Area_2 : %. 2f \n",Area_2);
 
-printf ("Informe o PIB 1:\n");  
+printf ("Informe o PIB 1:");
 scanf ("%f", &PIB_1);
-printf ("Exibindo valor real do PIB_1 : %f\n", PIB_1);
+printf ("Exibindo valor real do PIB_1 : %. 1f \n",PIB_1);
 
-printf ("Informe o PIB 2:\n");
-scanf ("%f", &PIB_2);
-printf ("Exibindo valor real do PIB 2 :%f\n", PIB_2);
+printf ("Informe o PIB 2:");
+scanf ("%f",&PIB_2);
+printf ("Exibindo valor real do PIB 2 : %. 1f \n",PIB_2);
 
-printf ("Informe a Densidade_populacional_1:\n");
+printf ("Informe a Densidade_populacional_1:");
 scanf ("%f", &Densidade_populacional_1);
-printf ("Exibindo valor real da densidade populacional 1 : %f\n", Densidade_populacional_1);
+printf ("Exibindo valor real da Densidade_populacional_1 : %. 2f \n",Densidade_populacional_1);
 
-printf ("Informe a densidade populacional 2:\n");
-scanf ("%f", &Densidade_populacional_2); 
-printf ("Exibindo valor real da densidade populacional 2 : %f\n", Densidade_populacional_2);
+printf ("Informe a Densidade_populacional_2:");
+scanf ("%f", &Densidade_populacional_2);
+printf ("Exibindo valor real da Densidade_populacional_2 : %. 2f \n",Densidade_populacional_2);
 
 
 
@@ -94,7 +96,7 @@ printf ("Exibindo valor real da densidade populacional 2 : %f\n", Densidade_popu
 
 
  return 0;
-}
+ }
 
 
 
@@ -102,3 +104,4 @@ printf ("Exibindo valor real da densidade populacional 2 : %f\n", Densidade_popu
 
 
 
+ 
